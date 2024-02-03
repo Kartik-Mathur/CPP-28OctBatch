5883e8eb425460387d9ffb21909662f6231c5515
Kartik <kartik.mathur10@gmail.com>
2024-02-03 15:35:22 +0530

    OOPs

--- L18-OOPs/OOPs.cpp
+++ L18-OOPs/OOPs.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 // ----------------------------- BLUEPRINT -----------------------------
 class Car {
+private:
+	int price;
+
 public:
 	char* name;
-	int price;
 	int model;
 	int seats;
 
@@ -55,6 +57,30 @@ public:
 		name = new char[strlen(n) + 1];
 		strcpy(name, n);
 	}
+
+
+	// Operator Overloading
+	void operator+=(Car &X) {
+		price += X.price;
+		model += X.model;
+		seats += X.seats;
+		char *newName = new char[strlen(name) + strlen(X.name) + 1];
+		strcpy(newName, name);
+		strcat(newName, X.name);
+		delete []name;
+		name = newName;
+	}
+
+
+	// GETTER AND SETTER
+	void setPrice(int p) {
+		if (p >= 100 and p <= 500) price = p;
+		else price = 350;
+	}
+
+	int getPrice() {
+		return price;
+	}
 };
 // ----------------------------- BLUEPRINT -----------------------------
 
@@ -62,18 +88,28 @@ int main() {
 
 	Car a;
 	// strcpy(a.name, "BMW");
-	a.setName("BMW");
-	a.price = 10;
+	char name[] = "BMW";
+	a.setName(name);
+	// a.price = 10;
+	a.setPrice(-1000);
+	cout << a.getPrice() << endl;
 	a.model = 2020;
 	a.seats = 5;
 
-	Car b("Maruti", 100, 2014, 5); // This will call parameterised constructor
-	b.setName("Tata");
+	Car b = a;
+	// Car b("Maruti", 100, 2014, 5); // This will call parameterised constructor
 	Car c(a);
+	Car d = a;
 
+	c.name[0] = 'T';
+
+	a += b;
 	a.print();
 	b.print();
 	c.print();
+	d.print();
+
+
 
 
 
